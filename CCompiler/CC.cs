6cb3bba00f2d2f5b337f64b7e5e3cd331190cadd
johan polson <mad_yuri_net@hotmail.com>
2018-07-20 19:55:48 +0200

    init

--- CCompiler/CC.cs
+++ CCompiler/CC.cs
@@ -132,47 +132,10 @@ namespace CCompiler
       }
     }
 
-    private static string GenerateExpression(Expression e)
-    {
-      if (e is Constant c)
-        return "$" + c.i;
-      if (e is UnaryOp u)
-      {
-        switch (u.type)
-        {
-          case UnaryOp.Type.Negation:
-            var ret = "\tmovl " + GenerateExpression(u.expression) + ", %ebx\n";
-            return ret + "\tneg %ebx\n";
-          default:
-            throw new NotImplementedException();
-        }
-      }
-      throw new NotImplementedException();
-    }
-    /*
-        RAX - 64 ------------ EAX - 32 ------ AX 16 AH 8, AL, 8
-
-        movl $2, %eax
-        neg %eax
-        ret
-        */
-
-    private static string GenerateStatement(Statement s)
-    {
-      return GenerateExpression(s.returnExp) +
-        "\tmovl %ebx, %eax\n" +
-        "\tret\n";
-    }
-
-    public static string GenerateFunction(Function f)
-    {
-      return "\t.globl\t_main\n" +
-             "_main:\n" + GenerateStatement(f.statementList[0]);
-    }
-
     public static string Generate(Program p)
     {
-      return GenerateFunction(p.functionList[0]);
+      var generate = new Generater(p);
+      return generate.Generate();
     }
 
     public static Program LexAndParse(string source)
